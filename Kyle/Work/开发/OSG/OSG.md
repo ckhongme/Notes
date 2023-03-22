022fa125908a9485ebfa38f490358b9cfd4505a0
kyle-CS <ckhongme@gmail.com>
2023-03-22 21:20:52 +0800

    osg的基础对象类

--- Kyle/Work/开发/OSG/OSG.md
+++ Kyle/Work/开发/OSG/OSG.md
@@ -2,17 +2,14 @@
 全称 open scene graph；一个开源的实时[[开发引擎#三维渲染引擎]];
 
 [[坐标系#世界坐标的朝向]]
-
 [[智能指针 ref_prt]]
 [[观察指针 observer_ptr]]
 
 [[节点Node]]
 [[Shader（osg)]]
 
-
-
 [[osgEarth]]
 
-
-
 空间参考系（Spatial Reference System，SRS）
+
+![[]]
---
+++ Kyle/Work/开发/OSG/osg脚本/csvrObjectEarth.h
@@ -0,0 +1,38 @@
+﻿//
+// Created by Kyle on 2023/3/20.
+//
+
+#ifndef CS_VR_DEMO_CSVROBJECTEARTH_H
+#define CS_VR_DEMO_CSVROBJECTEARTH_H
+
+#include <csvrBase/csvrObject.h>
+#include <osgEarth/MapNode>
+
+class csvrObjectEarth: public csvrObject
+{
+public:
+    csvrObjectEarth();
+    ~csvrObjectEarth();
+
+    ///初始化地球节点
+    void initMapNode(osgEarth::MapNode *pMapNode);
+    ///设置经纬度(大地坐标系初始化旋转矩阵*平移矩阵)
+    void setPosition(osg::Vec3d pos) override;
+
+    ///获取经纬度
+    osg::Vec3d getPostion() override;
+    ///获取本地欧拉角
+    osg::Vec3f getRotation() override;
+    void setTransformMatrix() override;
+
+    void rotateTo(osg::Vec3d to);
+    osgEarth::MapNode *pMapNode = nullptr; //地球节点
+
+private:
+    ///地理坐标系转世界坐标系矩阵：大地坐标系初始化旋转矩阵（朝北和垂直地心）*平移矩阵
+    void Earth2World(const osg::Vec3d& geoPos);
+    osg::Vec3d RadianLLH2XYZ(const osg::Vec3d& geoPos);
+    osg::Vec3d DegreeLLH2XYZ(const osg::Vec3d& geoPos);
+};
+
+#endif //CS_VR_DEMO_CSVROBJECTEARTH_H
